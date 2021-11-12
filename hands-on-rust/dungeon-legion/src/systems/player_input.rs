163949cd653afa62ebe52d5fe7cd134a48f17357
Clipsey <suno@planet.dev>
2021-11-12 07:32:08 -0800

    Turn based intent movement

--- hands-on-rust/dungeon-legion/src/systems/player_input.rs
+++ hands-on-rust/dungeon-legion/src/systems/player_input.rs
@@ -1,13 +1,12 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
     if let Some(key) = key {
@@ -21,17 +20,21 @@ pub fn player_input(
 
         if delta.x != 0 || delta.y != 0 {
             // let mut players = <(&mut Point, &Player)>::query();
-            let mut players = <&mut Point>::query().filter(component::<Player>());
+            // let mut players = <&mut Point>::query().filter(component::<Player>());
+            let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
 
-            // players.iter_mut(ecs).for_each(|(pos, _player)| {
-            players.iter_mut(ecs).for_each(|pos| {
+            // players.iter_mut(ecs).for_each(|((pos, _player)| {
+            players.iter(ecs).for_each(|(entity, pos)| {
                 let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: *entity,
+                        destination,
+                    },
+                ));
             });
+            *turn_state = TurnState::PlayerTurn;
         }
     }
 }
